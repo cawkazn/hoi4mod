989b2780d05fa1c7fb6f319a6bf1ba081d6e9b28
Corey Andersen <candersen@applied.local>
2018-11-26 11:18:09 -0600

    blah

---
+++ TagConverter/TagConverter/HelperModels/IdeologyProperties.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TagConverter.HelperModels
+{
+    public class IdeologyProperties
+    {
+        public string name { get; set; }
+        public string color { get; set; }
+        public string warImpactOnTension { get; set; }
+        public string factionImpactOnTension { get; set; }
+        public string factionTradeOpinion { get; set; }
+        public string aiType { get; set; }
+
+        public IdeologyProperties()
+        {
+
+        }
+
+        public IdeologyProperties(string name, string color, string warImpactOnTension, string factionImpactOnTension, string factiontTradeOpinion, string aiType)
+        {
+            this.name = name;
+            this.color = color;
+            this.warImpactOnTension = warImpactOnTension;
+            this.factionImpactOnTension = factionImpactOnTension;
+            this.factionTradeOpinion = factionTradeOpinion;
+            this.aiType = aiType;
+        }
+    }
+}
---
+++ TagConverter/TagConverter/HelperModels/IdeologyType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TagConverter.HelperModels
+{
+    public class IdeologyType
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+
+        public IdeologyType()
+        {
+
+        }
+
+        public IdeologyType(string name, string type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+    }
+}
